cc870496430fb11eb45664d28dcc81a8e6f12e0d
quochuy0510 <trinhhuy260298@gmail.com>
2026-06-08 11:02:03 +0800

    fix html

--- vite.config.js
+++ vite.config.js
@@ -6,13 +6,18 @@ import vueDevTools from 'vite-plugin-vue-devtools'
 
 // https://vite.dev/config/
 export default defineConfig({
+  base: '/vue-kkbox-cloning/',
+
   plugins: [
     vue(),
     vueDevTools(),
   ],
+
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
+      '@': fileURLToPath(
+        new URL('./src', import.meta.url)
+      )
     },
   },
 })
